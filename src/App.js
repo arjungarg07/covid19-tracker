ead6ea72702d3950f2ef387b93bfb4a640837d3a
arjungarg <arjunemailid@gmail.com>
2020-07-24 16:54:14 +0530

    Reconstruct Project Structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from "react";
 
+import { CountryPicker, Cards, Chart } from "./components";
+import styles from './App.module.css'
+import { fetchData } from './api'
 class App extends React.Component {
-  render(){
-    return(
-      <div>
-        Hello
+
+  async componentDidMount(){
+    const data = await fetchData(); 
+
+    console.log(data);
+  }
+    
+  render() {
+    return (
+      <div className = {styles.container}>  
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
-    )
+    );
   }
 }
 
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Card = ()=>{
+    return(
+        <h1>Card</h1>
+    )
+}
+
+export default Card;
